594b087eef8f3e49a4133d42fe9e93508518d7d5
rememberOk <2949448050@qq.com>
2018-05-13 22:46:41 +0800

    更新

--- app/src/main/java/com/example/test/Activity/VerificationActivity.java
+++ app/src/main/java/com/example/test/Activity/VerificationActivity.java
@@ -4,16 +4,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.example.test.R;
-import com.example.test.entity.User;
 import com.example.test.entity.Ver;
 import com.google.gson.Gson;
 import com.zhy.http.okhttp.OkHttpUtils;
